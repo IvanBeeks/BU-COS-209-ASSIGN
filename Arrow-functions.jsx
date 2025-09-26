efdd648a000bb2391f9649263419aab457ef4394
Ivan Ugwu <ivanmuna680@gmail.com>
2025-09-26 17:05:48 +0100

    Update Arrow-functions.jsx

---
+++ Arrow-functions.jsx
@@ -0,0 +1,7 @@
+// Arrow functions example
+const add = (a, b) => a + b;
+console.log(add(2, 3)); // 5
+
+const numbers = [1, 2, 3];
+const squared = numbers.map(n => n * n);
+console.log(squared); // [1, 4, 9]
